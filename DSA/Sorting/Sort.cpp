aff893a49c05e81d8a8d68a9a000336a7cc8be6d
Abhoy <abhoygorai04@gmail.com>
2022-05-06 22:23:07 +0530

    sorting

--- DSA/Sorting/Sort.cpp
+++ DSA/Sorting/Sort.cpp
@@ -2,19 +2,27 @@
 using namespace std;
 
 void BubSort(int *arr, int n){
+    int flag;
     for(int i = 0; i<n-1; i++){
+        flag = 0;
         for(int j = 0; j<n-1-i; j++){
-            if(arr[j] > arr[j+1])
+            if(arr[j] > arr[j+1]){
                 swap(arr[j], arr[j+1]);
+                flag = 1;
+            }
         }
+        if(flag == 0)
+            break;
     }
 }
 
+
+
 int main(){
+    // int arr[] = {1, 2, 3, 4, 5};
     int arr[] = {8, 5, 7, 3, 2};
     BubSort(arr, 5);
-    for(auto x:arr)
-    {
+    for(auto x:arr){
         cout<<x<<endl;
     }
 
