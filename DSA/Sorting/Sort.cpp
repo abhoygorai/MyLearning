00f7d34e95939cc764e44f3e22733dd724dbc23b
Abhoy <abhoygorai04@gmail.com>
2022-05-08 12:36:46 +0530

    Insertion sort

--- DSA/Sorting/Sort.cpp
+++ DSA/Sorting/Sort.cpp
@@ -16,12 +16,27 @@ void BubSort(int *arr, int n){
     }
 }
 
+void InsertionSort(int *arr, int n)
+{
+    int key, j;
+    for(int i=1; i<n; i++)
+    {
+        key = arr[i];
+        j = i-1;
 
+        while(j>=0 && arr[j]>key)
+        {
+            arr[j+1] = arr[j];
+            j=j-1;
+        }
+        arr[j+1] = key;
+    }
+}
 
 int main(){
     // int arr[] = {1, 2, 3, 4, 5};
     int arr[] = {8, 5, 7, 3, 2};
-    BubSort(arr, 5);
+    InsertionSort(arr, 5);
     for(auto x:arr){
         cout<<x<<endl;
     }
