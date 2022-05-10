b31a363af1990cae1e9ef0bb951547a0b864e782
Abhoy <abhoygorai04@gmail.com>
2022-05-10 21:25:48 +0530

    Practise

--- DSA/test.cpp
+++ DSA/test.cpp
@@ -2,25 +2,35 @@
 #include <fstream>
 using namespace std;
 
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+ 
+        /* Move elements of arr[0..i-1], that are
+        greater than key, to one position ahead
+        of their current position */
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
 int main()
 {
 
-    char ch;
-    streambuf *p;
-    ofstream os;
-    os.open("test.txt");
-    p = os.rdbuf();
-
-    do
-    {
-
-        ch = cin.get();
-
-        p->sputc(ch);
-
-    } while (ch != '.');
-
-    os.close();
+   int arr[] = {12, 11, 13, 5, 6};
+   insertionSort(arr, 5);
+   for(int x:arr)
+   {
+       cout<<x<<endl;
+   }
 
     return 0;
 }
